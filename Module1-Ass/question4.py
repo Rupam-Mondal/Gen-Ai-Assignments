6dee0e1792fd81f091a888ac2f324e8aed3a9b54
Rupam <rupammondal564@gmail.com>
2024-06-19 12:36:39 +0530

    assignment is done

---
+++ Module1-Ass/question4.py
@@ -0,0 +1,68 @@
+# 1. Arithmetic Operators
+#ex-->
+"""
+Arithmetic operators are used to perform basic 
+mathematical operations.
+ex--> +(Addition) , -(substraction) etc
+"""
+#code :-
+a = 3
+b = 5
+print(a + b)
+
+#2. Comparison operator
+"""
+Comparison operators are used to compare two values.
+ex--> ==, !=, >, 
+<, >=, <=
+"""
+# code:-
+a = 3
+b = 5
+print(a == b)  
+print(a != b)  
+print(a > b)   
+
+#3. Logical Operators
+"""
+Logical operators are used to combine conditional 
+statements.
+ex--> and, or, not
+"""
+#code:-
+print(a < b and a > 0)
+
+#4. Bitwise Operators
+"""
+Bitwise operators are used to perform bit-level 
+operations.
+ex--> &(Bitwise AND), |(Bitwise OR), ^(Bitwise XOR)
+"""
+#code:-
+print(a & b)
+
+#5. Assignment Operators
+"""
+Assignment operators are used to assign values to variables.
+ex--> =, +=, -= etc
+"""
+#code:-
+k = 5
+k += 3
+
+#6. Identity Operators
+"""
+Identity operators are used to compare the memory 
+locations of two objects.
+ex--> is, is not
+"""
+print(a is b)
+
+#7. Membership Operators
+"""
+Membership operators are used to test if a 
+sequence contains a value.
+ex--> in, not in
+"""
+list = [1 , 2 , 3 , 4 , 5]
+print(3 in list)
