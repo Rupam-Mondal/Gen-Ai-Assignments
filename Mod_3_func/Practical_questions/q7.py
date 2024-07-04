15ef3fe003dcb1de29fd09cdb2af8c7d23475e2b
Rupam <rupammondal564@gmail.com>
2024-07-04 21:09:40 +0530

    add

--- Mod_3_func/Practical_questions/q7.py
+++ Mod_3_func/Practical_questions/q7.py
@@ -1,2 +1,9 @@
-f = open("sample.txt" , "r")
-print(f.read())
+file_path = 'sample.txt'
+
+def line_read(file_path):
+    with open(file_path , 'r') as file:
+        for line in file:
+            yield line.strip()
+
+for line in line_read(file_path):
+    print(f"{line}")
